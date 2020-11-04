5fa0762e144e0a3e6c13645c1ede120a2b0c586f
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2020-11-04 16:43:37 +0000

    Update app.js
    SOLVED!

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const button = document.querySelector('button');
 
         tl.paused(true);
         tl.to(".overlay", {clipPath: 'circle(180%'})
-        tl.to("menu-container", {opacity: 1, y: '30px', stagger: 0.1}, "-=1")
+        tl.to(".menu-container", {opacity: 1, y: '30px', stagger: 0.1}, "-=1")
 
         button.addEventListener('click', () => {
             tl.play();
